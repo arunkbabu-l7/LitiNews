b5a878237d321e2e73e506062201ccd926926526
arunkbabu-l7 <arun.k@litmus7.com>
2022-01-28 12:48:44 +0530

    Minor Code Changes

--- app/src/main/java/com/litmus7/news/util/NewsEvent.kt
+++ app/src/main/java/com/litmus7/news/util/NewsEvent.kt
@@ -2,7 +2,6 @@ package com.litmus7.news.util
 
 import com.litmus7.news.domain.Article
 
-
 sealed class NewsEvent {
     class Success(val articles: List<Article>): NewsEvent()
     class Failure(val errorText: String): NewsEvent()
